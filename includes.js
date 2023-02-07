2461dd3ce39b63657a664e0ef4a595df1b16d315
ilan <ipfilus@gmail.com>
2023-02-06 21:21:57 -0300

    test

--- includes.js
+++ includes.js
@@ -3,7 +3,7 @@ const array = ['ilan', 'Bia', 'André', 1, 2, 3]
 console.log(array)
 
 const show = (name) => {
-    if(array[0].includes(name)) {
+    if(array.includes(name)) {
         console.log(`No array contém '${name}'.`)
     } else {
         console.log(`No array não contem '${name}'.`)
